5f785ab2a7edf2c8e1511a3b8242e2b2df2ddb1b
god <1159636109@qq.com>
2022-11-24 15:07:32 +0800

    push

--- src/main/java/com/atguigu/Main.java
+++ src/main/java/com/atguigu/Main.java
@@ -7,8 +7,7 @@ public class Main {
         System.out.println("Hello world3!");
         System.out.println("Hello world4!");
         System.out.println("master 1");
-
         System.out.println("hot fix 1");
-
+        System.out.println("Hello world5!");
     }
 }
